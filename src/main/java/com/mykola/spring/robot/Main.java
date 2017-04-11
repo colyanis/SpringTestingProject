53b5e4c5c72e9cad0e4fe7ac72515f9e166d589a
colyanis <colyanis@gmail.com>
2017-04-11 14:02:49 +0300

    Updated dao.

--- src/main/java/com/mykola/spring/robot/Main.java
+++ src/main/java/com/mykola/spring/robot/Main.java
@@ -3,6 +3,9 @@ package com.mykola.spring.robot;
 import com.mykola.spring.writer.Logger;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.dao.DataAccessException;
+
+import java.util.List;
 
 /**
  * Created by mtverdok on 4/10/2017.
@@ -10,9 +13,20 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class Main {
     public static void main(String[] args) {
         ApplicationContext context = new ClassPathXmlApplicationContext("robot.xml");
-        Robot robot = (Robot) context.getBean("robot");
+        OffersDAO offersDao = (OffersDAO) context.getBean("offersDao");
 
-        robot.speak();
+        try {
+            List<Offer> offers = offersDao.getOffers();
+
+            for(Offer offer : offers) {
+                System.out.println(offer);
+            }
+
+        } catch (DataAccessException e) {
+            System.out.println(e.getMessage());
+            System.out.println(e.getClass());
+        }
+
 
         ((ClassPathXmlApplicationContext) context).close();
     }
--- src/main/java/com/mykola/spring/robot/OffersDAO.java
+++ src/main/java/com/mykola/spring/robot/OffersDAO.java
@@ -1,12 +1,40 @@
 package com.mykola.spring.robot;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.stereotype.Component;
+
+import javax.sql.DataSource;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 
 /**
  * Created by mtverdok on 4/10/2017.
  */
+@Component("offersDao")
 public class OffersDAO {
+    private NamedParameterJdbcTemplate jdbc;
+
+    @Autowired
+    public void setDataSource(DataSource jdbc) {
+        this.jdbc = new NamedParameterJdbcTemplate(jdbc);
+    }
+
     public List<Offer> getOffers() {
-        return null;
+        MapSqlParameterSource params = new MapSqlParameterSource("name", "Mike");
+
+        return jdbc.query("select * from offers where name = :name", (resultSet, rowNum) -> {
+            Offer offer = new Offer();
+            offer.setId(resultSet.getInt("id"));
+            offer.setName(resultSet.getString("name"));
+            offer.setText(resultSet.getString("text"));
+            offer.setEmail(resultSet.getString("email"));
+
+            return offer;
+        });
     }
 }
